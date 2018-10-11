c23234017b184fe477fdfb77740fce9c34157b9e
Kohe Tokoi <tokoik@gmail.com>
2018-10-11 23:10:47 +0900

    GLFW によるテンプレートプログラムを追加します。

---
+++ main.cpp
@@ -0,0 +1,38 @@
+﻿//
+// メインプログラム
+//
+
+// メッセージボックスの表示の準備
+#if defined(_WIN32)
+#  include <Windows.h>
+#  include <atlstr.h>  
+#endif
+
+// アプリケーション本体
+#include "GgApplication.h"
+
+//
+// メインプログラム
+//
+int main() try
+{
+  // アプリケーション本体
+  GgApplication app;
+
+  // アプリケーションを実行する
+  app.run();
+}
+catch (const std::exception &e)
+{
+  // エラーメッセージを表示する
+#if defined(_WIN32)
+  const CStringW message(e.what());
+  MessageBox(NULL, LPCWSTR(message), TEXT("ゲームグラフィックス特論"), MB_OK | MB_ICONERROR);
+#else
+  std::cerr << e.what() << "\n\n[Type enter key] ";
+  std::cin.get();
+#endif
+
+  // ブログラムを終了する
+  return EXIT_FAILURE;
+}
