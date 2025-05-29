0e89da237c254b8adaa1db6ea11ab3c05557a9f6
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2025-05-30 08:40:23 +0900

    ウィンドウ関連の処理を追加します。

---
+++ Window.h
@@ -0,0 +1,143 @@
+﻿#pragma once
+
+///
+/// ウィンドウ関連の処理
+///
+/// @file Window.h
+/// @author Kohe Tokoi
+/// @date 2025/5/24
+///
+
+// OpenGL 関連
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+///
+/// ウィンドウ関連の処理クラス
+///
+class Window
+{
+  /// ウィンドウの識別子
+  GLFWwindow* const window;
+
+  /// ウィンドウの縦横比
+  GLfloat aspect;
+
+  ///
+  /// ウィンドウのサイズ変更時の処理
+  ///
+  /// @param[in] window サイズ変更の対象のウィンドウの識別子
+  /// @param[in] width サイズ変更後のウィンドウの幅
+  /// @param[in] height サイズ変更後のウィンドウの高さ
+  ///
+  static auto resize(GLFWwindow* window, int width, int height) -> void
+  {
+    // window が保持するインスタンスの this ポインタを得る
+    const auto instance{ static_cast<Window*>(glfwGetWindowUserPointer(window)) };
+
+    // インスタンスからの呼び出しでなければ戻る
+    if (instance == nullptr) return;
+
+    // このインスタンスが保持する縦横比を更新する
+    instance->aspect = static_cast<GLfloat>(width) / static_cast<GLfloat>(height);
+
+    // フレームバッファの大きさ
+    int fbWidth, fbHeight;
+
+    // フレームバッファの大きさを得る
+    glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+
+    // フレームバッファ全体をビューポートにする
+    glViewport(0, 0, fbWidth, fbHeight);
+  }
+
+public:
+
+  ///
+  /// コンストラクタ
+  ///
+  /// @param[in] width ウィンドウの幅
+  /// @param[in] height ウィンドウの高さ
+  /// @param[in] title ウィンドウのタイトル
+  ///
+  Window(int width = 640, int height = 480, const char* title = "GLFW Window")
+    : window{ glfwCreateWindow(width, height, title, nullptr, nullptr) }
+    , aspect{ static_cast<GLfloat>(width) / static_cast<GLfloat>(height) }
+  {
+    // ウィンドウが開けなければ戻る
+    if (window == nullptr) return;
+
+    // 現在のウィンドウを処理対象にする
+    glfwMakeContextCurrent(window);
+
+    // 表示はディスプレイのリフレッシュレートに同期させる
+    glfwSwapInterval(1);
+
+    // このインスタンスの this ポインタを記録しておく
+    glfwSetWindowUserPointer(window, this);
+
+    // ウィンドウのサイズ変更時に呼び出す処理を登録する
+    glfwSetWindowSizeCallback(window, resize);
+
+    // 開いたウィンドウに初期設定を適用する
+    resize(window, width, height);
+  }
+
+  // コピーコンストラクタは使用しない
+  Window(const Window& draw) = delete;
+
+  ///
+  /// デストラクタ
+  ///
+  virtual ~Window()
+  {
+    // ウィンドウを破棄する
+    glfwDestroyWindow(window);
+  }
+
+  // 代入演算子は使用しない
+  Window& operator=(const Window& draw) = delete;
+
+  ///
+  /// ウィンドウの識別子を取り出す
+  ///
+  /// @return ウィンドウの識別子
+  ///
+  auto get() const
+  {
+    return window;
+  }
+
+  ///
+  /// 描画ループの継続判定
+  ///
+  /// @return 描画を継続する場合は true
+  ///
+  explicit operator bool() const
+  {
+    // イベントを取り出す
+    glfwWaitEvents();
+
+    // ウィンドウを閉じる必要がなければ true を返す
+    return !glfwWindowShouldClose(window);
+  }
+
+  ///
+  /// ダブルバッファリング
+  ///
+  auto swapBuffers() const
+  {
+    // カラーバッファを入れ替える
+    glfwSwapBuffers(window);
+  }
+
+  ///
+  /// ウィンドウの縦横比を取り出す
+  ///
+  /// @return ウィンドウの縦横比
+  ///
+  auto getAspect() const
+  {
+    return aspect;
+  }
+};
