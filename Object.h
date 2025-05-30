aa3c11adc71c2ea3c58de35202b1ccc82fb0e2e8
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2025-05-30 11:12:57 +0900

    図形関連の処理を追加します。

---
+++ Object.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+///
+/// 図形関連の処理
+///
+/// @file object.h
+/// @author Kohe Tokoi
+/// @date 2025/5/29
+///
+
+// バッファオブジェクト関連の宣言は gl.h に含まれていないので glew.h を使う
+#include <GL/glew.h>
+
+///
+/// 頂点配列オブジェクト
+///
+struct Object
+{
+  /// 頂点配列オブジェクト名
+  const GLuint vao;
+
+  /// 頂点バッファオブジェクト名
+  const GLuint vbo;
+
+  ///
+  /// コンストラクタ
+  ///
+  /// @param[in] size データサイズ
+  /// @param[in] data データ
+  ///
+  Object(GLsizeiptr size, const void* data = nullptr);
+
+  // コピーコンストラクタは使用しない
+  Object(const Object& draw) = delete;
+
+  ///
+  /// デストラクタ
+  ///
+  virtual ~Object();
+
+  // 代入演算子は使用しない
+  Object& operator=(const Object& object) = delete;
+};
