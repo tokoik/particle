c5aefc8605191aaa9290bbfacf5146ea1c99ab59
Kohe Tokoi <tokoi@wakayama-u.ac.jp>
2025-05-30 11:12:57 +0900

    図形関連の処理を追加します。

---
+++ object.cpp
@@ -0,0 +1,40 @@
+﻿///
+/// 図形関連の処理の実装
+/// 
+/// @file object.cpp
+/// @author Kohe Tokoi
+/// @date 2025/5/29
+/// 
+#include "object.h"
+
+///
+/// 頂点配列オブジェクトの作成
+///
+/// @param[in] count 頂点数
+/// @param[in] position 頂点位置
+/// @return 作成された頂点配列オブジェクト名
+///
+auto createObject(GLuint count, const GLfloat(*position)[3]) -> GLuint
+{
+  // 頂点配列オブジェクトを作成して結合する
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // 頂点バッファオブジェクトを作成して結合する
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat[3]) * count, position, GL_DYNAMIC_DRAW);
+
+  // 結合されている頂点バッファオブジェクトを in 変数から参照できるようにする
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+  glEnableVertexAttribArray(0);
+
+  // 頂点配列オブジェクトの結合と頂点バッファオブジェクトの結合を解除する
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // 頂点配列オブジェクトの名前を返す
+  return vao;
+}
---
+++ object.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+///
+/// 図形関連の処理
+/// 
+/// @file object.h
+/// @author Kohe Tokoi
+/// @date 2025/5/29
+/// 
+
+// バッファオブジェクト関連の宣言は gl.h に含まれていないので glew.h を使う
+#include <GL/glew.h>
+
+///
+/// 頂点配列オブジェクトの作成
+///
+/// @param[in] count 頂点数
+/// @param[in] position 頂点位置
+/// @return 作成された頂点配列オブジェクト名
+///
+extern auto createObject(GLuint count, const GLfloat(*position)[3]) -> GLuint;
