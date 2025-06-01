c750ed65a3a11740eeaf25c13089ea5879644c2c
Kohe Tokoi <tokoik@gmail.com>
2025-06-01 17:41:15 +0900

    描画処理を追加します。

--- main.cpp
+++ main.cpp
@@ -15,6 +15,22 @@
 // OpenGL のエラーチェック
 #include "errorcheck.h"
 
+// シェーダの読み込み処理
+#include "shader.h"
+
+// 図形関連の処理
+#include "Object.h"
+
+// 粒子数
+const auto PARTICLE_COUNT{ 10000 };
+
+// 乱数
+#include <random>
+
+// GLM 関連
+#include <GLM/gtc/type_ptr.hpp>
+#include <GLM/gtc/matrix_transform.hpp>
+
 // 標準ライブラリ
 #include <iostream>
 
@@ -64,6 +80,43 @@ auto main() -> int
     return EXIT_FAILURE;
   }
 
+  // プログラムオブジェクトを作成する
+  const auto program{ loadProgram("point.vert", "point.frag") };
+
+  // プログラムオブジェクトが作成できなかったら
+  if (program == 0)
+  {
+    // エラーメッセージを出して
+    std::cerr << "Can't create program object." << std::endl;
+
+    // 終了する
+    return EXIT_FAILURE;
+  }
+
+  // uniform 変数 mc の場所を取得する
+  const auto mcLoc{ glGetUniformLocation(program, "mc") };
+
+  // 図形を作成する
+  Object object{ sizeof(glm::vec3) * PARTICLE_COUNT };
+
+  // 乱数生成器を初期化する
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+
+  // [-1.0f, 1.0f) の範囲の一様乱数の生成器を用意する
+  std::uniform_real_distribution<GLfloat> dist(-1.0f, 1.0f);
+
+  // 粒子の初期位置を設定する
+  glBindBuffer(GL_ARRAY_BUFFER, object.vbo);
+  const auto position{ static_cast<glm::vec3*>(glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY)) };
+  for (auto i{ 0 }; i < PARTICLE_COUNT; ++i)
+  {
+    // ランダムな位置を設定する
+    position[i] = { dist(engine), dist(engine), dist(engine) };
+  }
+  glUnmapBuffer(GL_ARRAY_BUFFER);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
   // 背景色を指定する
   glClearColor(0.2f, 0.3f, 0.4f, 0.0f);
 
@@ -72,6 +125,24 @@ auto main() -> int
   {
     // ウィンドウを消去する
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // プログラムオブジェクトを指定する
+    glUseProgram(program);
+
+    // ビュー変換行列を設定する
+    const auto view{ glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f)) };
+
+    // 投影変換行列を設定する
+    const auto projection{ glm::perspective(glm::radians(60.0f), window.getAspect(), 1.0f, 5.0f) };
+
+    // uniform 変数 mc に値を設定する
+    glUniformMatrix4fv(mcLoc, 1, GL_FALSE, glm::value_ptr(projection * view));
+
+    // 図形を指定する
+    glBindVertexArray(object.vao);
+
+    // 図形を描画する
+    glDrawArrays(GL_POINTS, 0, PARTICLE_COUNT);
 
     // OpenGL のエラーが発生していないかチェックする
     errorcheck();
